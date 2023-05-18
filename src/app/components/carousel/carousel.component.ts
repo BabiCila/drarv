6f42c42588caa2ca8852099019c779c28c947bc0
BarbaraM <babicilagrr@gmail.com>
2023-05-18 20:08:35 +0100

    implements the carousel component

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,14 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
 
+export class CarouselComponent {
+    images = [`../../../assets/img/carousel1.svg`,`../../../assets/img/carousel2.svg`,`../../../assets/img/carousel3.svg`]
 }
+
